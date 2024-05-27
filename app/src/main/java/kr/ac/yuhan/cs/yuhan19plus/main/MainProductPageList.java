6e2dc49886f0ec045abc47f23450e3dee14a14f1
Jungmin <junminlee828@gmail.com>
2024-05-27 21:05:52 +0900

    사용자 관련 페이지 UI 일부 수정 및 액티비티(이정민)한부분 주석 맨트 추가

--- app/src/main/java/kr/ac/yuhan/cs/yuhan19plus/main/MainProductPageList.java
+++ app/src/main/java/kr/ac/yuhan/cs/yuhan19plus/main/MainProductPageList.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
